e20a79b76d683da544c6296b15474fc031ecc229
Diego Cajal <dcajal95@gmail.com>
2023-07-05 14:11:04 +0200

    Cloud Storage trigger -> Check if PPG file. If PPG, generate a numpy array with PPG data

--- functions/main.py
+++ functions/main.py
@@ -1,14 +1,17 @@
-import pathlib
+import os
+
+import firebase_admin
 import numpy as np
 
 from firebase_functions import storage_fn
-from firebase_admin import initialize_app
+from firebase_admin import credentials, storage
 from scipy.interpolate import CubicSpline
-# from lib.delineation import ppg_pulse_detection
 # from lib.filters import filtering_and_normalization, remove_impulse_artifacts
+# from lib.delineation import ppg_pulse_detection
 # from lib.hrv import time_metrics
 
-initialize_app()
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {'storageBucket': 'bsicos-app.appspot.com'})
 
 
 @storage_fn.on_object_finalized(region="europe-west1")
@@ -17,7 +20,7 @@ def process_signal(
 ):
     """When a file is uploaded in the Storage bucket, check if PPG and process."""
 
-    file_path = pathlib.PurePath(event.data.name)
+    file_path = event.data.name
 
     # Exit if this is triggered on a file that is not a csv file.
     if not file_path.endswith("hz.csv"):
@@ -25,15 +28,23 @@ def process_signal(
         return
     
     print(f"Processing PPG file. ({file_path})")   
-    fs = 250
 
     # Load file
-    my_file = np.genfromtxt(file_path, delimiter=',')
-    data_matrix = np.delete(my_file, 0, 0)
+    bucket = storage.bucket()
+    file_blob = bucket.get_blob(file_path)
+    file_text = file_blob.download_as_text()
+    file = open('temp.txt', 'w')  # Create 'temp.txt' file
+    file.write(file_text)
+    file.close()
+
+    # Generate data matrix and separate arrays
+    data_matrix = np.loadtxt(file.name, delimiter=',', skiprows=1)
+    os.remove(file.name)  # Remove 'temp.txt' file
     green = data_matrix[:, 1]
     unixtimestamps = data_matrix[:, 3]
 
     # Interpolate ppg at fs
+    fs = 250
     t_aux = (unixtimestamps - unixtimestamps[0]) / 1000
     t = np.arange(0, t_aux[-1], 1 / fs)
     cs = CubicSpline(t_aux, -green)
@@ -42,10 +53,10 @@ def process_signal(
     # Baseline removal, filtering and normalization of PPG signal
     # ppg_filtered = filtering_and_normalization(ppg, fs)
     # ppg_filtered = remove_impulse_artifacts(ppg_filtered)
-
-    # Pulse detection
+    #
+    # # Pulse detection
     # ppg_tk = ppg_pulse_detection(ppg_filtered, fs, plotflag=False)
-
-    # HRV
+    #
+    # # HRV
     # time_metrics(ppg_tk)
     # ppg_tn = gap_correction(ppg_tk, True)
